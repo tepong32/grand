c08ffb235953e994211ae1b358bfd27023ee7a6d
teppy <tepong32@gmail.com>
2024-09-23 12:35:28 +0800

    removed 'departments' on fieldsets

--- users/admin.py
+++ users/admin.py
@@ -8,7 +8,7 @@ from .models import User, Department, Manager, Profile, Salary, SalaryIncrement
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'is_active', 'is_staff', 'is_superuser','last_login', 'date_joined')
     fieldsets = (
-        (None, {'fields': ('username', 'password', 'department', 'groups')}),
+        (None, {'fields': ('username', 'password', 'groups')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'middle_name', 'ext_name', 'email', )}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
     )
