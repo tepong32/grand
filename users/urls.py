66ae5d941237ec1cb2dea7ebacf263582e5fc52f
teppy <tepong32@gmail.com>
2024-10-04 10:48:00 +0800

    modified user_search_view to allow searching for users using username or email addresses. corrected url routing, template mods

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,11 @@ from .views import *
 
 ### complete this some other time ###
 urlpatterns = [
+    path('search/', user_search_view, name='user-search' ), # this should be first as urls catches errors whtn using the search function if it's not
     path('', usersIndexView, name='users-list'),
     path('<username>/', profileView, name='profile' ),
     path('<username>/edit/', profileEditView, name='profile-edit' ),
+
 
 ]
 
--- users/views.py
+++ users/views.py
@@ -97,17 +97,30 @@ def profileEditView(request, username=None):
 
 ### accounts/users searching view
 ### not used ATM
+
+from django.db.models import Q  # This is needed for the search query to work properly
+                                # allowing the search to use matches on usersnames OR email addresses
+                                # as opposed to the original username AND email:
+                                # "search_results = User.objects.filter(username__icontains=search_query).filter(email__icontains=search_query).distinct()"
+
 def user_search_view(request, *args, **kwargs):
     context = {}
     if request.method == "GET":
+        print("Request GET:", request.GET)  # Add this line
         search_query = request.GET.get("q")
-        if len(search_query) > 0:
-            search_results = User.objects.filter(username__icontains=search_query).filter(email__icontains=search_query).distinct()
-            user = request.user
-            accounts = [] # [(account1, True), (account2, False), ...]
-            for account in search_results:
-                accounts.append((account, False)) # you have no friends yet
-            context['accounts'] = accounts
+        print("Search query:", search_query)  # Add this line
+        try:
+            if len(search_query) > 0:
+                search_results = User.objects.filter(Q(username__icontains=search_query) | Q(email__icontains=search_query)).distinct() # match EITHER useranme OR email
+                user = request.user
+                accounts = [] # [(account1, True), (account2, False), ...]
+                for account in search_results:
+                    accounts.append((account, False)) # you have no friends yet
+                context['accounts'] = accounts
+        except Exception as e:
+            print("Error:", e)
+            print("Request GET:", request.GET)
+            print("Search query:", search_query)
                 
     return render(request, "users/user_search_results.html", context)
 
