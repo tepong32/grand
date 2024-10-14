c26bf5f1ee3ae61682b2f5a889ebb01cebc52ceb
teppy <tepong32@gmail.com>
2024-10-14 16:38:30 +0800

    added CRUD views for Leave organized model linkings updated urls (but not the templates yet)

---
+++ leave_mgt/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Leave
+
+class LeaveForm(forms.ModelForm):
+    class Meta:
+        model = Leave
+        fields = ('leave_type', 'date_filed', 'number_of_days', 'notes')
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # remove 'SP' option if the user is not a superuser
+        # so only admins can use the special leave option
+        if not user.is_superuser:
+            # this translates to: display (key, value) for key, value in Leave.LEAVE_TYPES if key is != 'SP'
+            # it loops through the choices for the attr and displays all other choices ASIDE FROM 'SP'
+            self.fields['leave_type'].choices = [(k, v) for k, v in Leave.LEAVE_TYPES if k != 'SP']
--- leave_mgt/views.py
+++ leave_mgt/views.py
@@ -1,11 +1,13 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 
 from users.models import User, Profile
-from .models import LeaveCredits
+from .forms import LeaveForm
+from .models import Leave, LeaveCredits
 
 from django.views.generic import (
     TemplateView,
@@ -54,3 +56,103 @@ class MyLeaveView(LoginRequiredMixin, RoleBasedTemplateMixin, TemplateView):
         context['leave_credits'] = leave_credits
 
         return context
+
+class LeaveApplicationCreateView(CreateView, LoginRequiredMixin):
+    form_class = LeaveForm
+    template_name = 'leave_mgt/leave_application.html'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        leave_type = form.cleaned_data['leave_type']
+        number_of_days = form.cleaned_data['number_of_days']
+        employee = self.request.user.profile.leavecredits
+
+        # Check leave credits based on leave type
+        if leave_type == 'SL':
+            if employee.current_year_sl_credits <= 0 or (employee.current_year_sl_credits - number_of_days < 0):
+                messages.error(self.request, "Insufficient Sick Leave credits.")
+                return self.form_invalid(form)
+
+        elif leave_type == 'VL':
+            if employee.current_year_vl_credits <= 0 or (employee.current_year_vl_credits - number_of_days < 0):
+                messages.error(self.request, "Insufficient Vacation Leave credits.")
+                return self.form_invalid(form)
+
+        # If checks pass, create the leave record
+        form.instance.employee = employee  # Set the employee field
+        response = super().form_valid(form)
+
+        # Update leave credits only if the leave is approved
+        if form.instance.status == 'APPROVED':
+            if leave_type == 'SL':
+                employee.current_year_sl_credits -= number_of_days
+            elif leave_type == 'VL':
+                employee.current_year_vl_credits -= number_of_days
+            employee.save()  # Save the updated leave credits
+
+        messages.success(self.request, "Leave application submitted successfully.")
+        return redirect('success_view')  # Redirect to a success page
+
+class LeaveApplicationUpdateView(UpdateView):
+    model = Leave
+    form_class = LeaveForm
+    template_name = 'leave_application.html'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        leave_type = form.cleaned_data['leave_type']
+        number_of_days = form.cleaned_data['number_of_days']
+        employee = self.request.user.profile.leavecredits
+
+        # Check leave credits based on leave type
+        if leave_type == 'SL':
+            if employee.current_year_sl_credits <= 0 or (employee.current_year_sl_credits - number_of_days < 0):
+                messages.error(self.request, "Insufficient Sick Leave credits.")
+                return self.form_invalid(form)
+
+        elif leave_type == 'VL':
+            if employee.current_year_vl_credits <= 0 or (employee.current_year_vl_credits - number_of_days < 0):
+                messages.error(self.request, "Insufficient Vacation Leave credits.")
+                return self.form_invalid(form)
+
+        # If checks pass, update the leave record
+        response = super().form_valid(form)
+
+        # Update leave credits only if the leave is approved
+        if form.instance.status == 'APPROVED':
+            if leave_type == 'SL':
+                employee.current_year_sl_credits -= number_of_days
+            elif leave_type == 'VL':
+                employee.current_year_vl_credits -= number_of_days
+            employee.save()  # Save the updated leave credits
+
+        messages.success(self.request, "Leave application updated successfully.")
+        return redirect('success_view')  # Redirect to a success page
+
+
+class LeaveApplicationDeleteView(DeleteView):
+    model = Leave
+    template_name = 'leave_mgt/leave_application_delete.html'
+    success_url = reverse_lazy('leave_list')  # Redirect to a list view after deletion
+
+    def delete(self, request, *args, **kwargs):
+        leave = self.get_object()
+        employee = request.user.profile.leavecredits
+
+        # If the leave is approved, add back the number of days to the corresponding credits
+        if leave.status == 'APPROVED':
+            if leave.leave_type == 'SL':
+                employee.current_year_sl_credits += leave.number_of_days
+            elif leave.leave_type == 'VL':
+                employee.current_year_vl_credits += leave.number_of_days
+            employee.save()  # Save the updated leave credits
+
+        return super().delete(request, *args, **kwargs)
