71458fe8c4c66dae87507aac22bd0a32b2796c94
teppy <tepong32@gmail.com>
2024-10-14 14:21:43 +0800

    removed 'if settings.DEBUG' line since its only needed on the main urls.py

--- home/urls.py
+++ home/urls.py
@@ -33,11 +33,3 @@ urlpatterns = [
     ] 
 
     # replaced <int:pk> with <str:slug>
-
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -11,10 +11,3 @@ urlpatterns = [
 
 
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
