e6f403f7576232366f367810a4b189c84a308e84
teppy <tepong32@gmail.com>
2024-10-16 16:20:35 +0800

    added detailview

--- leave_mgt/urls.py
+++ leave_mgt/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', MyLeaveView.as_view(), name="leave_list"),
     path('create/', LeaveApplicationCreateView.as_view(), name='leave_create'),
+    path('<pk>/update/', LeaveApplicationDetailView.as_view(), name='leave_detail'),
     path('<pk>/update/', LeaveApplicationUpdateView.as_view(), name='leave_update'),
     path('<pk>/delete/', LeaveApplicationDeleteView.as_view(), name='leave_delete'),
 
