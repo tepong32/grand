86a5fc7dc2341ba34ddc6d7719c9f705e13c96e3
teppy <tepong32@gmail.com>
2024-10-29 15:02:53 +0800

    corrected url path for leave detail view, removed it from urls for the meantime as well.

--- leave_mgt/urls.py
+++ leave_mgt/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', MyLeaveView.as_view(), name="leave_list"),
     path('create/', LeaveApplicationCreateView.as_view(), name='leave_create'),
-    path('<pk>/update/', LeaveApplicationDetailView.as_view(), name='leave_detail'),
+    # path('<pk>/detail/', LeaveApplicationDetailView.as_view(), name='leave_detail'), # technically, not needed
     path('<pk>/update/', LeaveApplicationUpdateView.as_view(), name='leave_update'),
     path('<pk>/delete/', LeaveApplicationDeleteView.as_view(), name='leave_delete'),
 
