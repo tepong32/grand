bac5d36df9f4cb5f8131e4a9dfd304c5abac7651
teppy <tepong32@gmail.com>
2024-11-04 10:07:35 +0800

    added/removed global context variables

--- leave_mgt/context_processors.py
+++ leave_mgt/context_processors.py
@@ -1,5 +1,5 @@
 from users.models import User, Profile
-from .models import LeaveCredits, LeaveRequest
+from .models import LeaveCredits, LeaveRequest, LeaveCreditLog
 
 from django.utils import timezone
 
@@ -12,17 +12,18 @@ def dashboard_context(request):
 		user = request.user
 		# Fetch the user-specific data you need
 		leave_credits = LeaveCredits.objects.get(employee=request.user.profile)
-		cy_sl = leave_credits.current_year_sl_credits
-		cy_vl = leave_credits.current_year_vl_credits
-		pending_leaves = LeaveRequest.objects.filter(employee=leave_credits, status='PENDING')
-		pending_leaves_count = sum(leave.number_of_days for leave in pending_leaves)	# of days count
-		approved_leaves = LeaveRequest.objects.filter(employee=leave_credits, status='APPROVED')
-		approved_leave_count = sum(leave.number_of_days for leave in pending_leaves)	# of days count
+		cy_remaining_sl = leave_credits.current_year_sl_credits
+		cy_remaining_vl = leave_credits.current_year_vl_credits
+		pending_leaves = LeaveRequest.objects.filter(employee=leave_credits, status='PENDING') # assigning variable to all instances of pending leaves for us
+		pending_leaves_count = sum(leave.number_of_days for leave in pending_leaves)	# to get their number_of_days total count
+		approved_leaves = LeaveRequest.objects.filter(employee=leave_credits, status='APPROVED') # assigning variable to all instances of approved leaves for us
+		approved_leave_count = sum(leave.number_of_days for leave in pending_leaves)	# to get their number_of_days total count
 
 		# Get the current year
 		current_year = timezone.now().year
 		# Get the count of approved leave requests for the current year
 		current_yr_leave_usage = get_leave_usage(leave_credits, current_year)
+		accrual_logs = LeaveCreditLog.objects.filter(leave_credits=leave_credits)
 
 		return {
 			'approved_leaves': approved_leaves,
@@ -30,8 +31,8 @@ def dashboard_context(request):
 
 			'current_year': current_year,
 			'current_yr_leave_usage': current_yr_leave_usage,
-			'cy_sl': cy_sl,
-			'cy_vl': cy_vl,
+			'cy_sl': cy_remaining_sl,
+			'cy_vl': cy_remaining_vl,
 			'leave_credits': leave_credits,
 
 			'pending_leaves': pending_leaves,
