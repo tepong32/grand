70d6411b154d1c000e8eb35b3586b27039160d20
teppy <tepong32@gmail.com>
2025-01-17 13:03:53 +0800

    added an 'unauthedhome' view

--- home/views.py
+++ home/views.py
@@ -52,6 +52,49 @@ class HomeView(LoginRequiredMixin, ListView):
             'draft': draft
         })
         return context
+
+
+class unauthedHomeView(LoginRequiredMixin, ListView):
+    model = Announcement
+    template_name = 'home/unauthed/home.html'
+    context_object_name = 'announcements'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # user = User # not being used atm
+
+        # Fetch public announcements and order them by created_at
+        public = Announcement.objects.filter(announcement_type=Announcement.PUBLIC).order_by('-created_at')
+        internal = Announcement.objects.filter(announcement_type=Announcement.INTERNAL).order_by('-created_at')
+
+        # Get the latest 5 public announcements for the carousel
+        latest_public = public[:5]  # Slicing to get the latest 5
+        # Filter out the latest announcements from the public list
+        remaining_public = public[5:]  # Get all public announcements except the latest 5
+
+        # Optionally, if you need published and draft announcements for admin posting purposes
+        published = Announcement.objects.filter(published=True)
+        draft = Announcement.objects.filter(published=False)
+
+        # manually iterated latest announcements for chaotic positioning on the bulletin board
+        # you can just use 'latest_public' if you are only to iterate them over using a for-loop
+        latest_positions = [
+            {'announcement': latest_public[0], 'top': '30px', 'left': '30px'},
+            {'announcement': latest_public[1], 'top': '100px', 'left': '220px'},
+            {'announcement': latest_public[2], 'top': '170px', 'left': '145px'},
+            {'announcement': latest_public[3], 'top': '230px', 'left': '120px'},
+            {'announcement': latest_public[4], 'top': '300px', 'left': '160px'},
+        ]
+        context.update({
+            'public': public,
+            'latest_public': latest_public,
+            'latest_positions': latest_positions,
+            'remaining_public': remaining_public,
+            'internal': internal,
+            'published': published,
+            'draft': draft
+        })
+        return context
         
 class AnnouncementList(ListView):
     model = Announcement
