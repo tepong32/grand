577eee9e3df3f9b54fe8b20ae86efcebaf392fe9
tEppy <tepong32@gmail.com>
2025-03-30 21:28:44 +0800

    missing argument 'sociallogin' might need to fully read allauth docs regarding overriding this adapter thing.

--- users/adapters.py
+++ users/adapters.py
@@ -3,14 +3,26 @@ from allauth.socialaccount.providers.google.provider import GoogleProvider
 from .models import User
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    '''
-    Custom adapter to handle user population from social accounts.
-    This adapter is used to populate the user instance with data from the social account.
-    It is particularly useful for Google accounts where we want to extract specific fields.
-    '''
-    def populate_user(self, request, sociallogin, **kwargs):
+    def new_user(self, request, sociallogin):
+        # Create a new user instance
+        user = super().new_user(request, sociallogin)
+
+        # Populate user fields with data from Google
+        if sociallogin.account.provider == GoogleProvider.id:
+            extra_data = sociallogin.account.extra_data
+            user.first_name = extra_data.get('given_name', '')
+            user.last_name = extra_data.get('family_name', '')
+            user.email = sociallogin.account.email  # Ensure the email is set
+
+            # Optionally, you can set middle_name and ext_name if available
+            # user.middle_name = extra_data.get('middle_name', '')
+            # user.ext_name = extra_data.get('ext_name', '')
+
+        return user
+
+    def populate_user(self, request, sociallogin, data=None):
         # Call the parent method to get the user instance
-        user = super().populate_user(request, sociallogin, **kwargs)
+        user = super().populate_user(request, sociallogin, data)
 
         # Get the data from the social account
         if sociallogin.account.provider == GoogleProvider.id:
