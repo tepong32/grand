7f9c3f57755daca3f4509ea162946cdf6ff9ae38
tEppy <tepong32@gmail.com>
2025-06-06 20:30:29 +0800

    added import

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
+from django import forms
 from .models import EmployeeProfile
 
 class ProfileUpdateForm(forms.ModelForm):
 	class Meta:
 		model = EmployeeProfile
-		fields = ["image", "note", "contact_number", "address"] # attrs editable by the user, others are for HR/admin edits only
+		fields = ["profile_image", "note", "contact_number", "address"] # attrs editable by the user, others are for HR/admin edits only
