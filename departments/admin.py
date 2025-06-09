74c7b3c02e273f5e7d93b2286c69661c6f38668a
teppy <tepong32@gmail.com>
2025-06-09 11:28:05 +0800

    arranged department display to alphabetical

--- departments/admin.py
+++ departments/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Department, Plantilla
 # Register your models here.
 
-admin.site.register(Department)
-admin.site.register(Plantilla)
+from django.contrib import admin
+from .models import Department
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'deptHead_or_oic']
+    ordering = ['name']  # ✅ alphabetical by department name
+
+@admin.register(Plantilla)
+class PlantillaAdmin(admin.ModelAdmin):
+    # list_display = ['title', 'item_number', 'salary_grade', 'department']
+    ordering = ['title']  # ✅ alphabetical by department name
+
+# admin.site.register(Department)
+# admin.site.register(Plantilla)
