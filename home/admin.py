dbb5809f67ad6ffb54fd3d5f06fd7e96da5f379f
tEppy <tepong32@gmail.com>
2025-06-16 00:47:32 -0400

    changes on nc server

--- home/admin.py
+++ home/admin.py
@@ -9,11 +9,11 @@ class AnnouncementAdmin(admin.ModelAdmin):
 
 @admin.register(OrgPersonnel)
 class OrgPersonnelAdmin(admin.ModelAdmin):
-    list_display = ('title', 'name')
+    list_display = ('title', 'name', 'display_order')
 
 @admin.register(DepartmentContact)
 class DepartmentContactAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'landline', 'mobile', 'email', 'messenger_chat_link')
 
 
 @admin.register(DownloadableForm)
