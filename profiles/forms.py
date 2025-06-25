6c8e7810cea5aec12f7953cc3b9dadbd11a7beac
teppy <tepong32@gmail.com>
2025-06-25 10:49:29 +0800

    modified views and templates to display info based on whether employee or citizen profile is being displayed, pre-modularization-(includes blocks)-of-templates push

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import EmployeeProfile
+from .models import EmployeeProfile, CitizenProfile
 
 class ProfileUpdateForm(forms.ModelForm):
 	class Meta:
 		model = EmployeeProfile
 		fields = ["profile_image", "note", "contact_number", "address"] # attrs editable by the user, others are for HR/admin edits only
+
+
+class CitizenProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CitizenProfile
+        fields = ['contact_number', 'address']
--- profiles/views.py
+++ profiles/views.py
@@ -30,26 +30,49 @@ def profileView(request, username=None):
 
 @login_required
 def profileEditView(request, username=None):
+    '''
+    Provide a form for the user to edit their profile information.
+    Form that will be displayed will depend on whether the user is a Citizen or Employee.
+    '''
     user = get_object_or_404(User, username=username)
 
     if user != request.user:
         return render(request, "profiles/error.html", {"error": "You do not have permission to edit this profile."})
 
+    # Check if user is a Citizen or Employee
+    is_employee = hasattr(user, 'employeeprofile')
+    is_citizen = hasattr(user, 'citizenprofile')
+
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.employeeprofile)
+
+        if is_employee:
+            p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.employeeprofile)
+        elif is_citizen:
+            from .forms import CitizenProfileUpdateForm
+            p_form = CitizenProfileUpdateForm(request.POST, instance=request.user.citizenprofile)
+        else:
+            return render(request, "profiles/error.html", {"error": "Profile type not recognized."})
 
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f"Account info has been updated.")
-            return render(request, "profiles/profile.html", {"user": request.user})
+            messages.success(request, "Account info has been updated.")
+            return redirect("profile", slug=request.user.citizenprofile.slug if is_citizen else request.user.employeeprofile.slug)
+
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.employeeprofile)
+        if is_employee:
+            p_form = ProfileUpdateForm(instance=request.user.employeeprofile)
+        elif is_citizen:
+            from .forms import CitizenProfileUpdateForm
+            p_form = CitizenProfileUpdateForm(instance=request.user.citizenprofile)
 
     context = {
         'u_form': u_form,
-        'p_form': p_form
+        'p_form': p_form,
+        'is_employee': is_employee,
+        'is_citizen': is_citizen
     }
     return render(request, 'profiles/profile_edit.html', context)
+
