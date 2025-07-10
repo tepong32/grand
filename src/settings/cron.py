abee297b9b045a0cca358c360501fc6b91e611db
tEppy <tepong32@gmail.com>
2025-07-10 19:34:59 +0800

    changed cron args setting to just two instead of three because of crontab package parsing error bug

--- src/settings/cron.py
+++ src/settings/cron.py
@@ -1,8 +1,9 @@
 CRONJOBS = [
     # minute hour day month weekday <command-to-execute>
-    # ''' this is running daily at midnight '''
-    ('0 0 * * *', 'leave_mgt.cron.update_leave_credits_from_cronPy', '>> /logs/cron.log 2>&1'), # prod path: /home/abutdtks/test.abutchikikz.online/logs/cron.log 2>&1'
-    ('@reboot', 'telegram_bot.cron.StartTelegramBotCron', '>> /logs/telegram_cron.log 2>&1'), # running tg bot on server startup/restarts
-]
 
-CRON_CLASSES = ['django_crontab.crontab.CronJobBase']
+    # Run daily at midnight
+    ('0 0 * * *', 'leave_mgt.cron.update_leave_credits_from_cronPy >> /home/abutdtks/test.abutchikikz.online/logs/cron.log 2>&1'),
+
+    # Run Telegram bot on system reboot
+    ('@reboot', 'telegram_bot.cron.StartTelegramBotCron >> /home/abutdtks/test.abutchikikz.online/logs/telegram_cron.log 2>&1'),
+]
